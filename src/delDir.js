2ec3ed3a6e2fdbbea74d42d4bf43cc84af69bc1e
rip <371662584@qq.com>
2020-03-18 09:07:26 +0800

    clean output dir

---
+++ src/delDir.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+module.exports = function delDir(path){
+    let files = [];
+    if(fs.existsSync(path)){
+        files = fs.readdirSync(path);
+        files.forEach((file, index) => {
+            let curPath = path + "/" + file;
+            if(fs.statSync(curPath).isDirectory()){
+                delDir(curPath); //递归删除文件夹
+            } else {
+                fs.unlinkSync(curPath); //删除文件
+            }
+        });
+        fs.rmdirSync(path);
+    }
+}
